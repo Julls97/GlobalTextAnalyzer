a41eaae5c091f03ae833f1a85d7dadcca13a2812
Julls <yulia.jolie@yandex.ru>
2018-12-18 18:02:22 +0500

    pattern to split

--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -6,27 +6,8 @@ import javax.jws.WebService;
 import javax.xml.ws.Endpoint;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-/*
-String makeCaps (String text) - возвращает исходный текст, записанный ПРОПИСНЫМИ буквами.
-
-Int countOneWord (String text, String word) - возвращает количество слов word в тексте text.
-
-(uChar, Int) [] countCharacters (String text) - возвращает массив, содержащий набор пар (символ, количество),
-отражающий информацию, по количеству использования каждого символа в тексте.
-
-(String, Int) [] countWords (String text) - возвращает массив,
-содержащий набор пар (слово, количество), отражающий информацию,
-по количеству использования каждого из слов в тексте.
-
-
-Разместить разработанный веб-сервис на облачной платформе.	* 2 балла
-Реализовать метод (String Int) [] countWordsOnWebPage (String URL),
-возвращающий информацию по количеству слов на веб-странице с адресом URL	 * 3 балла
-*/
 
 @WebService()
 public class HelloWorld {
@@ -43,7 +24,7 @@ public class HelloWorld {
 	
 	@WebMethod
 	public int countOneWord(@WebParam(name = "text") String text, @WebParam(name = "word") String word) {
-		Pattern pattern = Pattern.compile(word);
+		Pattern pattern = Pattern.compile(word + " ");
 		Matcher matcher = pattern.matcher(text);
 		int count = 0;
 		while (matcher.find()) count++;
@@ -54,21 +35,25 @@ public class HelloWorld {
 	public Pair[] countCharacters(@WebParam(name = "text") String text) {
 		HashMap<Character, Integer> collect = new HashMap<Character, Integer>();
 		for (Character c : text.toLowerCase().toCharArray())
-			collect.put(c, collect.containsKey(c) ? ( collect.get(c) + 1 ) : 1);
+			//if(!(c == ' '))
+				collect.put(c, collect.containsKey(c) ? ( collect.get(c) + 1 ) : 1);
 		
-		ArrayList<Entry<Character, Integer>> array = new ArrayList<>();
-		array.addAll(collect.entrySet());
+		ArrayList<Pair> array = new ArrayList<>();
+		Character[] keys = collect.keySet().toArray(new Character[0]);
+		for (Character key: keys) array.add(new Pair(key.toString(), collect.get(key)));
 		return array.toArray(new Pair[0]);
 	}
 	
 	@WebMethod
 	public Pair[] countWords(@WebParam(name = "text") String text) {
 		HashMap<String, Integer> collect = new HashMap<String, Integer>();
-		for (String c : text.toLowerCase().split(" "))
+		for (String c : text.toLowerCase().split("[\\p{P}\\p{N}\\s]+"))
+//		for (String c : text.toLowerCase().split("[\\p{P}\\s]+"))
 			collect.put(c, collect.containsKey(c) ? ( collect.get(c) + 1 ) : 1);
 		
-		ArrayList<Entry<String, Integer>> array = new ArrayList<>();
-		array.addAll(collect.entrySet());
+		ArrayList<Pair> array = new ArrayList<>();
+		String[] keys = collect.keySet().toArray(new String[0]);
+		for (String key: keys) array.add(new Pair(key, collect.get(key)));
 		return array.toArray(new Pair[0]);
 	}
 }
